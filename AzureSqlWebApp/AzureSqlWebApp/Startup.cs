df08724f7d27d53c88b1eed9f6c1231e17dcf542
Aman020 <amanbhayana@yahoo.in>
2018-01-29 22:20:04 +0530

    Initial Commit

---
+++ AzureSqlWebApp/AzureSqlWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureSqlWebApp.Startup))]
+namespace AzureSqlWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
